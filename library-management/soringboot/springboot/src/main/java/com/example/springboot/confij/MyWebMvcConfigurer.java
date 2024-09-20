b24e3ad7b3064597ef987cc5828215e293f06846
陆天睿 <2076765633@qq.com>
2024-09-20 21:27:22 +0800

    Initial commit

---
+++ library-management/soringboot/springboot/src/main/java/com/example/springboot/confij/MyWebMvcConfigurer.java
@@ -0,0 +1,11 @@
+//package com.example.springboot.confij;
+//
+//
+//import org.springframework.beans.factory.annotation.Configurable;
+//
+//@Configurable
+//
+//public class MyWebMvcConfigurer {
+//
+//
+//}
---
+++ library-management/soringboot/src/test/java/com/example/soringboot/SoringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
